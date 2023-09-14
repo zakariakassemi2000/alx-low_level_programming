edb08378d8480e75019be5b1551c4ff02d13309d
zakaria <zakariakassemi65@gmail.com>
2023-09-14 11:06:25 -0700

    kii

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -1,14 +1,9 @@
-#include "main.h"
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+int mul(int a, int b);
+
+/* Add other function prototypes here if needed */
+
+#endif /* MAIN_H */
 }
